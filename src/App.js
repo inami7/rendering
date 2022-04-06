126eaf6622a5719d6470fb8744e1ff98192b3bf8
inami7 <103008905+inami7@users.noreply.github.com>
2022-04-06 15:17:30 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+//useCallback   アロー関数を囲うことで再生成を防ぐ
+//              第二引数に見張る値を設定するとその値が変わったときに再生成される
+
+import { useState, useCallback } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+  const [text, setText] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
---
+++ src/ChildArea.jsx
@@ -0,0 +1,30 @@
+//memo  囲ったpropsが変更されない限り再レンダリングしない
+//      基本的にコンポーネントは囲ったほうがいい
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("レンダリングされた");
+
+  const data = [...Array(2).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
